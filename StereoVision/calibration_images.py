024c12882d562f9df8fdca33c6bb51dd711d2948
nikhilg2603 <121499292+nikhilg2603@users.noreply.github.com>
2024-02-28 01:02:29 +0530

    Update calibration_images.py

--- StereoVision/calibration_images.py
+++ StereoVision/calibration_images.py
@@ -1,40 +1,27 @@
 import cv2
 
-# Chessboard parameters
-chessboard_size = (6, 5)  # Define the number of inner corners in the chessboard pattern
 
-# Initialize video capture object
 cap = cv2.VideoCapture(0)
+cap2 = cv2.VideoCapture(2)
 
-# Initialize variable for image numbering
 num = 0
 
+
 while cap.isOpened():
-    success, img = cap.read()
 
-    # Convert frame to grayscale
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    succes1, img = cap.read()
+    succes2, img2 = cap2.read()
 
-    # Find chessboard corners
-    ret, corners = cv2.findChessboardCorners(gray, chessboard_size, None)
 
-    if ret:
-        # Refine corner positions
-        corners = cv2.cornerSubPix(gray, corners, (11, 11), (-1, -1), (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001))
+    k = cv2.waitKey(5)
 
-        # Draw corners on the image
-        img_with_corners = cv2.drawChessboardCorners(img, chessboard_size, corners, True)
-
-        cv2.imshow('Camera with Corners', img_with_corners)
-
-    # Save image when 's' key is pressed
-    key = cv2.waitKey(5)
-    if key == 27:  # ESC key
+    if k == 27:
         break
-    elif key == ord('s'):
-        cv2.imwrite('images/stereo/image' + str(num) + '.png', img_with_corners)
-        print("Image saved!")
+    elif k == ord('s'): # wait for 's' key to save and exit
+        cv2.imwrite('images/stereoLeft/imageL' + str(num) + '.png', img)
+        cv2.imwrite('images/stereoRight/imageR' + str(num) + '.png', img2)
+        print("images saved!")
         num += 1
 
-cap.release()
-cv2.destroyAllWindows()
+    cv2.imshow('Img 1',img)
+    cv2.imshow('Img 2',img2)
